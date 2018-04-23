0f00ba0ad47aac4cb6d8bd314c8d7db369140fb6
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-04-23 14:48:20 +0200

    directorio project

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
+    url(r'^$', 'calc.views.barra'),
+    url(r'^(-?\d+)/(.+)/(-?\d+)$', 'calc.views.calcular'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
